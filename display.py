fd92d962fb86cf171b9edb344fd3f4e503b32a6a
Michael Fishman <michaeljfishman@gmail.com>
2019-01-28 18:52:13 -0500

    Tweaked display.

--- display.py
+++ display.py
@@ -8,18 +8,32 @@ image_directory = os.path.dirname(os.path.realpath(__file__)) + "/images/"
 # img=mpimg.imread(image_directory + "face.png")
 
 def displayDistribution(distribution, items):
+	# plt.tick_params(
+	#     axis='x',          # changes apply to the x-axis
+	#     which='both',      # both major and minor ticks are affected
+	#     bottom=False,      # ticks along the bottom edge are off
+	#     top=False,         # ticks along the top edge are off
+	#     labelbottom=False) # labels along the bottom edge are off
 	num_items = distribution.shape[0]
 	# bar_width = 1.0/(1.5 * num_items)
 	bar_width = 0.05
 	locs = [float(i + 1.0)/(num_items+1)-0.05 for i in range(num_items)]
 	fig, ax = plt.subplots()
+
 	ax.set_xlim(left=0,right=1)
-	a=ax.get_xaxis()
+	ax.set_ylim(bottom=0,top=1)
+	ax.xaxis.set_ticks_position("none")
+	ax.yaxis.set_ticks_position("none")
+	# ax.spines['bottom'].set_visible(False)
+	ax.spines['top'].set_visible(False)
+	ax.spines['right'].set_visible(False)
+	ax.xaxis.set_ticklabels(["" for i in ax.xaxis.get_ticklabels()])
+
 	# print(a)
 	# print("figure width: {}".format(fig.patch.get_width()))
 	bar = ax.bar(locs,distribution,bar_width, align='edge')
 	image_axis_height = 0.0
-	image_x_offset = -0.022
+	image_x_offset = -0.028
 	# image_axis_leftmost = 0.4
 	# image_increment = 0.8 / num_items
 	# bar_transform = bar.patches.get_patch_transform()
@@ -37,6 +51,13 @@ def displayDistribution(distribution, items):
 		imgplot	 = a_i.imshow(img)
 	ax.axison=True
 	plt.show()
+def displayDistributionList(distribution_list,items, observations = None):
+	"""	Displays a list of distributions. Navigate between distributions using left, right. 
+	Displays observation on plot as well if observations is not None.
+	If it becomes sluggish, rewrite to reuse previously created graphs
+	for when a human is providing observations and the distribution list is created iteratively"""
+	pass
+
 if __name__ == "__main__":
 	# A = np.arange(25).reshape(5,5)
 	# fig, ax = plt.subplots(1, 1)
